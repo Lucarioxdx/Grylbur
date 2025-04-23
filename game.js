5b7909d53af7bf870c355e70c6e7edb6bb4481ff
Lucas L. <lucasleite3010@gmail.com>
2025-04-23 16:48:27 -0300

    colisões2

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: {y: 0},
-            debug: false,
+            debug: true,
         }
     },
     scene: {
@@ -46,65 +46,68 @@ function create () {
     player = this.physics.add.sprite (400, 400, 'jogador');
     player.setScale(2);
     player.setCollideWorldBounds (true)
-
+    player.setSize (player.width*1.1, player.height*1.1).setOffset (player.width*0.01, player.height*0.01)
     enemy = this.physics.add.sprite (1400, 600, 'inimigo');
     enemy.setScale(4);
     enemy.setCollideWorldBounds (true)
 
+    obstacles = this.physics.add.staticGroup ();
+    obstacles.create(450, 205, 'arvore1').setScale(4).refreshBody();
+  
+    obstacles.create(678, 456, 'arvore2').setScale(5).refreshBody();
+  
+    obstacles.create(1790, 556, 'arvore3').setScale(6).refreshBody();
+    
+    obstacles.create(120, 456, 'arvore1').setScale(4).refreshBody();
+   
+    obstacles.create(890, 678, 'arvore2').setScale(5).refreshBody();
+ 
+    obstacles.create(23, 78, 'arvore3').setScale(6).refreshBody();
+  
+    obstacles.create(789, 67, 'arvore1').setScale(4).refreshBody();
 
-    tree1 = this.physics.add.sprite (450, 205, 'arvore1');
-    tree1.setScale(4);
-    tree2 = this.physics.add.sprite (678, 456, 'arvore2');
-    tree2.setScale(5);
-    tree3 = this.physics.add.sprite (1790, 556, 'arvore3');
-    tree3.setScale(6);
-    tree4 = this.physics.add.sprite (120, 456, 'arvore1');
-    tree4.setScale(4);
-    tree5 = this.physics.add.sprite (890, 678, 'arvore2');
-    tree5.setScale(5);
-    tree6 = this.physics.add.sprite (23, 78, 'arvore3');
-    tree6.setScale(6);
-    tree7 = this.physics.add.sprite (789, 67, 'arvore1');
-    tree7.setScale(4);
-    tree8 = this.physics.add.sprite (35, 890, 'arvore2');
-    tree8.setScale(5);
-    tree9 = this.physics.add.sprite (1000, 234, 'arvore3');
-    tree9.setScale(6);
-    tree10 = this.physics.add.sprite (1400, 200, 'arvore1');
-    tree10.setScale(4);
-    tree11 = this.physics.add.sprite (346, 967, 'arvore2');
-    tree11.setScale(5);
-    tree12 = this.physics.add.sprite (753, 74, 'arvore3');
-    tree12.setScale(6);
-    tree13 = this.physics.add.sprite (1000, 234, 'arvore3');
-    tree13.setScale(6);
-    tree14 = this.physics.add.sprite (697, 374, 'arvore1');
-    tree14.setScale(4);
-    tree15 = this.physics.add.sprite (369, 209, 'arvore2');
-    tree15.setScale(5);
-    tree16 = this.physics.add.sprite (1232, 346, 'arvore3');
-    tree16.setScale(6);
-    tree17 = this.physics.add.sprite (1000, 234, 'arvore3');
-    tree17.setScale(6);
-    tree18 = this.physics.add.sprite (967, 486, 'arvore1');
-    tree18.setScale(4);
-    tree19 = this.physics.add.sprite (436, 236, 'arvore2');
-    tree19.setScale(5);
-    tree20 = this.physics.add.sprite (457, 789, 'arvore3');
-    tree20.setScale(6);
-    tree21 = this.physics.add.sprite (1567, 486, 'arvore1');
-    tree21.setScale(4);
-    tree22 = this.physics.add.sprite (1234, 890, 'arvore2');
-    tree22.setScale(5);
-    tree23 = this.physics.add.sprite (1900, 234, 'arvore3');
-    tree23.setScale(6);
+    obstacles.create(35, 890, 'arvore2').setScale(5).refreshBody();
+   
+    obstacles.create(1000, 234, 'arvore3').setScale(6).refreshBody();
+  
+    obstacles.create(1400, 200, 'arvore1').setScale(4).refreshBody();
+ 
+    obstacles.create(346, 967, 'arvore2').setScale(5).refreshBody();
+    
+    obstacles.create(753, 74, 'arvore3').setScale(6).refreshBody();
+   
+    obstacles.create(1000, 234, 'arvore1').setScale(6).refreshBody();
+ 
+    obstacles.create(697, 374, 'arvore1').setScale(4).refreshBody();
+    
+    obstacles.create(369, 209, 'arvore2').setScale(5).refreshBody();
+    
+    obstacles.create(1232, 346, 'arvore3').setScale(6).refreshBody();
+   
+    obstacles.create(1000, 234, 'arvore1').setScale(6).refreshBody();
+    
+    obstacles.create(967, 486, 'arvore1').setScale(4).refreshBody();
+   
+    obstacles.create(436, 236, 'arvore2').setScale(5).refreshBody();
+    
+    obstacles.create(457, 789, 'arvore3').setScale(6).refreshBody();
+    
+    obstacles.create(1567, 486, 'arvore1').setScale(4).refreshBody();
+  
+    obstacles.create(1234, 890, 'arvore2').setScale(5).refreshBody();
+    
+    obstacles.create(1900, 234, 'arvore3').setScale(6).refreshBody();
+    
+
+    this.physics.add.collider (player, obstacles);
+    this.physics.add.collider (enemy, obstacles);
+    
+   
+
     cursors = this.input.keyboard.createCursorKeys();
 
-    obstacles = this.physics.add.staticGroup ();
-    obstacles.addMultiple ([tree1, tree2, tree3, tree4, tree5,  tree6,  tree7,  tree8,  tree9,  tree10,  tree11,  tree12,  tree13,  tree14,  tree15,  tree16,  tree17,  tree18,  tree19,  tree20,  tree21,  tree22,  tree23])
-
-    this.physics.add.collider (enemy, obstacles);
-    this.
+   
+    
 }
 
 
